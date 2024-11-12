5af66f7c006df857ebe232a28326c29d2b50d3b6
aizako <aizako96@gmail.com>
2024-11-12 20:20:37 +0300

    test point on a map

--- app/src/main/java/com/sergeyapp/gpsnavigation_test/MainActivity.kt
+++ app/src/main/java/com/sergeyapp/gpsnavigation_test/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.sergeyapp.gpsnavigation_test
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.sergeyapp.gpsnavigation_test.databinding.ActivityMainBinding
 import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.MapObjectTapListener
+import com.yandex.runtime.image.ImageProvider
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +26,28 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initView()
+    }
+
+    private val placemarkTapListener = MapObjectTapListener { _, point ->
+        Toast.makeText(
+            this@MainActivity,
+            "Tapped the point (${point.longitude}, ${point.latitude})",
+            Toast.LENGTH_SHORT
+        ).show()
+        true
+    }
+
+    private fun initView() = with(viewBinding) {
+
+        val imageProvider = ImageProvider.fromResource(this@MainActivity, R.drawable.ic_map_point)
+
+        val placemark = mapView.map.mapObjects.addPlacemark().apply {
+            geometry = Point(55.751225, 37.62954)
+            setIcon(imageProvider)
+        }
+        placemark.addTapListener(placemarkTapListener)
     }
 
     override fun onStart() {
